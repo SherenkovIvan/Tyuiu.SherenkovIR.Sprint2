41d4e9814b6ce86b88b0a5ad189d2b252f6fcc8f
Sherenkov Ivan <sherenkov12@gmail.com>
2025-10-04 14:09:28 +0500

    Add project files.

---
+++ Tyuiu.SherenkovIR.Sprint2.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SherenkovIR.Sprint2.Task0.V5.Lib;
+namespace Tyuiu.SherenkovIR.Sprint2.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 105;
+            int y = 223;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, true, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint2.Task0.V5/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.SherenkovIR.Sprint2.Task0.V5.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #0| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
+// Длинна строки 75 символов
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #2                                        *");
+Console.WriteLine("* Тема: Создание итогового решения по спринту      *");
+Console.WriteLine("* Задание #1                                       *");
+Console.WriteLine("* Вариант #5                                       *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("* Написать программу из операций сравнений         *");
+Console.WriteLine("*(==, !=, <, >, <=, >=, последовательность операций*");
+Console.WriteLine("* не должна нарушаться) и арифметических выражений,*");
+Console.WriteLine("* которая вернет логическую последовательность     *");
+Console.WriteLine("* (массив) ((False, True, True, True, False, True) *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+
+int x = 105;
+int y = 223;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("****************************************************");
+Console.WriteLine("*РЕЗУЛЬТАТ:                                        *");
+Console.WriteLine("****************************************************");
+
+for (int i=0; i<6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
