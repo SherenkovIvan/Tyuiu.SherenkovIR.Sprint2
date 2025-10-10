61af465f79bdc31e2101522ef130d4feab03316b
Sherenkov Ivan <sherenkov12@gmail.com>
2025-10-10 18:42:40 +0500

    Task3

--- Tyuiu.SherenkovIR.Sprint2.Task3.V24.Lib/DataService.cs
+++ Tyuiu.SherenkovIR.Sprint2.Task3.V24.Lib/DataService.cs
@@ -6,32 +6,28 @@ namespace Tyuiu.SherenkovIR.Sprint2.Task3.V24.Lib
         public double Calculate(double x)
         {
             double y = 0;
-            if ( x > 0)
+
+            if (x > 0)
             {
-                y = x * Math.Pow((10 + Math.Sin(Math.Sqrt(x) + 1)) / x, x);
+                y = x * Math.Pow((10 + Math.Sin(Math.Sqrt(x + 1))) / x, x);
             }
-            else
+            else if (x == 0)
             {
-                if (x == 0) 
-                {
-                   y = Math.Cos(x);
-                }
-                else
-                {
-                    if (x >= 0 || x <= -28)
-                    {
-                        y = 1 + 1 / (x * x);
-                    }
-                    else
-                    {
-                        if (x < -28)
-                        {
-                            y = x * x +10 * x - 1.0 / x;
-                        }
-                    }
-                }
+                y = Math.Cos(x) + (12 / (x * x)); 
             }
+            else if (x > -28 && x < 0) 
+            {
+                y = Math.Pow(1 + 1 / (x * x), x);
+            }
+            else 
+            {
+                y = x * x + 10 * x - (1.0 / x);
+            }
+
             return Math.Round(y, 3);
         }
+
+           
+        
     }
 }
