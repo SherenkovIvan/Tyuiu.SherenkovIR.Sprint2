79eb3fbeecfb2645719bf4610f5cd1356df6a205
Sherenkov Ivan <sherenkov12@gmail.com>
2025-10-11 15:12:14 +0500

    Task4

---
+++ Tyuiu.SherenkovIR.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.SherenkovIR.Sprint2.Task4.V11.Lib;
+namespace Tyuiu.SherenkovIR.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 22;
+            double y = 20;
+            double res = ds.Calculate(x,y);
+            double wait = 3891794346.101;
+            Assert.AreEqual(wait, res );
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 7;
+            double y = 16;
+            double res = ds.Calculate(x, y);
+            double wait = 100500885.959;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SherenkovIR.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.SherenkovIR.Sprint2.Task4.V11.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #2| Выполнил: Шеренков И. Р. | ИБКСБ-25-1";
+// Длинна строки 75 символов
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #2                                        *");
+Console.WriteLine("* Тема:* Тернарный оператор                        *");
+Console.WriteLine("* Задание #4                                       *");
+Console.WriteLine("* Вариант #11                                      *");
+Console.WriteLine("* Выполнил: Шеренков Иван Романович | ИБКСБ-25-1   *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое  *");
+Console.WriteLine("* значение с использованием тернарного оператора   *");
+Console.WriteLine("* где пользователь вводит значение переменных      *");
+Console.WriteLine("* x, y с клавиатуры.         Ответ окгруглить до 3 *");
+Console.WriteLine("* знаков после запятой.                            *");
+
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+
+Console.WriteLine("Введите значение переменной X");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной y");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double res = ds.Calculate(x, y);
+
+Console.WriteLine("****************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                       *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("Значение функции = " + res);
+
+Console.ReadKey();
